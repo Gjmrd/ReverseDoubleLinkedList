7335ae598f0be8b12ab997492aef03b28b55506d
Babaev Mahmoud <mahmud1997babaev@gmail.com>
2020-01-21 08:47:47 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace DoublyLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var node1 = new Node() { Value = 1 };
+            var node2 = new Node() { Value = 2, Prev = node1 };
+            var node3 = new Node() { Value = 3, Prev = node2 };
+            node1.Next = node2;
+            node2.Next = node3;
+
+            var head = Reverse(node1);
+
+            while (head != null)
+            {
+                Console.WriteLine(head.Value);
+                head = head.Next;
+            }
+        }
+
+        static Node Reverse(Node head)
+        {
+            if (head == null || head.Next == null)
+                return null;
+
+            Node temp = null;
+            Node current = head;
+            Node newHead = head;
+
+            while (current != null)
+            {
+                if (current.Next != null && current.Next.Prev != current)
+                    return null;
+
+                temp = current.Next;
+                current.Next = current.Prev;
+                current.Prev = temp;
+                newHead = current;
+                current = current.Prev;
+            }
+
+            Console.WriteLine();
+            return newHead;
+        }
+    }
+
+    class Node
+    {
+        public int Value;
+        public Node Prev;
+        public Node Next;
+    }
+}
